10a07b7a3922453a3749e6bd7f7a22aeee94d947
arshadfaislabadi <102044245+arshadfaislabadi@users.noreply.github.com>
2022-03-23 16:42:05 +0500

    spesyfyingattribues1234jsx

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import  "./App.css";
+
+const el = <h1 className="bg">Hellow arshad ali is</h1>;
+
+export default el;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+import el from "./App";
+//one only one element render
+ReactDOM.render(el, document.getElementById("root"))
+//componenrt how to render complete
+//ReactDOM.render(<App name="Rahual" />, document.getElementById("root"));
